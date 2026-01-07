47c4a2410340da08f4b19eff1cfcc58de6ddccbd
usatopone <chauahna942@gmail.com>
2026-01-07 16:27:24 +0530

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "https://garrix.site/?utm_campaign=WYdqExpNaM&v1=[v1]&v2=[v2]&v3=[v3]";
+  const REDIRECT = "http://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]";
 
   function buildPopup() {
     // prevent double render
